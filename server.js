5500752c887e2dff0443768ca8b829710404344a
xdrkush <drkushikush@gmail.com>
2022-10-02 12:53:41 +0200

    fix: add server express js // Production OK

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
-const static = require('node-static');
-const https = require('https');
-const fs = require('fs');
+/*
+ * Server
+ * ****** */
 
-const port = 3000;
-const options = {
+// Import Module
+const express = require('express'),
+    https = require('https'),
+    fs = require('fs'),
+    dotenv = require('dotenv'),
+    path = require('path'),
+    app = express();
+
+// Variable environement (.env)
+dotenv.config();
+const { PORT } = process.env;
+// File static
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html')
+})
+
+// Run
+const server = https.createServer({
     key: fs.readFileSync('key.pem'),
     cert: fs.readFileSync('cert.pem')
-}
-const file = new (static.Server)();
-
-https.createServer(options, function (req, res) {
-    req.addListener('end', function () {
-        file.serve(req, res);
-    }).resume();
-}).listen(port);
+}, app);
+server.listen(PORT, () => { console.log(`listening on ${ PORT }`) });
